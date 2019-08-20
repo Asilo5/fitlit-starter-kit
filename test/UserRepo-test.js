121c7b90df054deb0fd5d8ea7f3bf844bd740d51
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-08-20 16:01:42 -0600

    Start with testing

---
+++ test/UserRepo-test.js
@@ -0,0 +1,18 @@
+ const chai = require('chai');
+ const expect = chai.expect;
+
+ const mockData = require('../data/mock-user');
+ const UserRepo = require('../src/UserRepo');
+
+
+ describe('UserRepo', () => {
+
+  it('should be a function', () => {
+
+  })
+
+  it('should be an instance of UserRepo', () => {
+
+})
+
+ });
