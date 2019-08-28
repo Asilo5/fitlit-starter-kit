bbc5f7046e75109a4bc9928c99bb335fa6728f4a
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-08-27 20:55:36 -0600

    Add style to inputs

--- src/scripts.js
+++ src/scripts.js
@@ -6,14 +6,29 @@ let sleep = new Sleep(sleepData);
 let activity = new Activity(activityData);
 
 $('.first-name').html(user.returnFirstName());
+$('.user-address').html(user.address);
+$('.email-address').html(user.email);
+$('.stride-length').html(user.strideLength);
 
-$('.id-from-user').html(user.id);
+// $('.id-from-user').html(user.id);
 
 $('.day-water-consumption').html(hydration.dayFluidOz(user.id, "2019/06/17") )
 
-
+$('.daily-water-per-week').append(`
+      <tr>
+        <th>Day 1</th>
+         <th>Day 2</th>
+         <th>Day 3</th>
+         <th>Day 4</th>
+         <th>Day 5</th>
+         <th>Day 6</th>
+         <th>Day 7</th>
+      <tr>`)
 hydration.fluidOzWeekly(user.id).forEach((ounce) => {
-  $('.daily-water-per-week').append(`<p> ${ounce} </p>`);
+  $('.daily-water-per-week').append(`
+    <tr>
+      <td>${ounce}oz</td>
+    <tr>`)
 });
 
 $('.avg-users-stepgoal').html(userRepo.returnAvgStepGoal());
@@ -21,12 +36,39 @@ $('.avg-users-stepgoal').html(userRepo.returnAvgStepGoal());
 $('.hours-sleep').html(sleep.hoursSleptOnDay(user.id, "2019/06/17"));
 $('.quality-sleep').html(sleep.qualitySleptOnDay(user.id, "2019/06/17"));
 
+
+$('.sleep-hrs-weekly').append(`
+      <tr>
+        <th>Day 1</th>
+         <th>Day 2</th>
+         <th>Day 3</th>
+         <th>Day 4</th>
+         <th>Day 5</th>
+         <th>Day 6</th>
+         <th>Day 7</th>
+      <tr>`)
 sleep.hoursSleptWeekly(user.id, "2019/06/16").forEach((hour) => {
-  $('.sleep-hrs-weekly').append(`<p> ${hour} </p>`);
+  $('.sleep-hrs-weekly').append(`
+  <tr>
+      <td>${hour}hrs</td>
+   <tr>`);
 });
 
+$('.sleep-quality-weekly').append(`
+      <tr>
+        <th>Day 1</th>
+         <th>Day 2</th>
+         <th>Day 3</th>
+         <th>Day 4</th>
+         <th>Day 5</th>
+         <th>Day 6</th>
+         <th>Day 7</th>
+      <tr>`)
 sleep.qualitySleptWeekly(user.id, "2019/06/16").forEach((quality) => {
-  $('.sleep-quality-weekly').append(`<p> ${quality} </p>`);
+  $('.sleep-quality-weekly').append(`
+    <tr>
+       <td>${quality}</td>
+   <tr>`);
 });
 
 
